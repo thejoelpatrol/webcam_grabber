7b1c9996d50133e274b35be144c149abe707a1ec
Joel <jcretan@gmail.com>
2014-09-09 00:13:36 -0700

    basically works, but mplayer only reliably captures frames when launched every 2 seconds

--- grab.c
+++ grab.c
@@ -1,26 +1,40 @@
+//#include <limits.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#define FRAMES 100
+#define FRAMES 10
 #define FPS 2
+#define MPLAYER_DEFAULT_FILENAME "00000001.png"
+
 
 int main (int argc, char *argv[]) {
-	for (int i = 0; i < FRAMES; i++) {
-		char *argv_mplayer[7];
-		argv_mplayer[0] = "mplayer";
-		argv_mplayer[1] = "-vo";
-		argv_mplayer[2] = "png";
-		argv_mplayer[3] = "-frames";
-		argv_mplayer[4] = "1";
-		argv_mplayer[5] = "tv://";
-		argv_mplayer[6] = NULL;
+	char *argv_mplayer[7];
+	argv_mplayer[0] = "mplayer";
+	argv_mplayer[1] = "-vo";
+	argv_mplayer[2] = "png";
+	argv_mplayer[3] = "-frames";
+	argv_mplayer[4] = "1";
+	argv_mplayer[5] = "tv://\n";
+	argv_mplayer[6] = NULL;
 
+	for (int i = 0; i < FRAMES; i++) {
 		pid_t mplayer = fork();
-		if (mplayer > 0)
+		if (mplayer > 0) {
 			execvp("mplayer", argv_mplayer);	
-		
+			return -1;
+		}
+
 		int status;
 		waitpid(mplayer, &status, 0);
+		if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+			char filename[FILENAME_MAX];
+			sprintf(filename, "%d.png", i);
+			int stat = rename(MPLAYER_DEFAULT_FILENAME, filename);
+		}
+		else if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			printf("!! status: %d\n", status);
+		sleep(2);
 	}
 
 
